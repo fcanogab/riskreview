898a75429eb9ef22952e90c32b26bdf825715fde
Florencio Cano Gabarda <fcanogab@redhat.com>
2024-09-17 13:30:38 +0200

    Add initial models

--- risks/models.py
+++ risks/models.py
@@ -1,3 +1,122 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
+
+
+class Component(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="components")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('component_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    components = models.ManyToManyField(Component, related_name="attributes")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('attribute_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
+
+
+class Threat(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    attributes = models.ManyToManyField(Attribute, related_name="threats")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('attribute_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
+
+
+class Control(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    threats = models.ManyToManyField(Attribute, related_name="controls")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('control_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
+
+
+class ImplementedControl(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    control = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="implemented_controls")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('control_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    components = models.ManyToManyField(Component, related_name="categories")
+    threats = models.ManyToManyField(Threat, related_name="categories")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('control_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['name']
